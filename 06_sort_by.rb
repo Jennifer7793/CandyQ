d4d32c3d02425abf0e96a667801ea6f285d4a9fa
Jennifer <jennifer7793@gmail.com>
2021-11-24 18:54:50 +0800

    sort_by

--- 06_sort_by.rb
+++ 06_sort_by.rb
@@ -1,13 +1,23 @@
 # 排序規則：[1, 2, 4, 3, 5, 8, 7, 9, 6]
 
-list1 = [a, b, c, d]
-list2 = [i, b, f, e]
-list3 = [g, a, e, c]
+list1 = [1, 2, 4, 3]
+list2 = [2, 5, 8, 6]
+list3 = [1, 4, 5, 7]
 
 def sort_by_rule(list)
-  list [0..8] = [a, b, c, d, e, f, g, i]
-  
+  main = [1, 2, 4, 3, 5, 8, 7, 9, 6]
+  for i in main 
+    for x in list
+      if i == x
+        [] << list
+      end
+    end
+  end
+  list
 end
+  # list = main - list
+  # list = main - list
+
 
 p sort_by_rule(list1)  # 印出 [1, 2, 4, 3]
 p sort_by_rule(list2)  # 印出 [2, 5, 8, 6]
